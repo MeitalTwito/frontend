99d2cedda7fce2653eb09ecf7e013d8de04dc368
Meital Twito <tw.meital@gmail.com>
2022-07-12 08:12:01 +0300

    Added Toy Details & Edit

--- src/services/toy-service.js
+++ src/services/toy-service.js
@@ -6,20 +6,47 @@ const KEY = 'toyDB';
 
 export const toyService = {
     query,
+    remove,
+    getById,
+    getEmptyToy,
+    save
 }
 
 _createToys()
+
 function query() {
     return storageService.query(KEY)
 }
 
+function remove(id) {
+    return storageService.remove(KEY, id)
+}
 
+function getById(id) {
+    return storageService.get(KEY, id)
+    .then(toy => {
+        toy.reviews = _createreviews()
+        return toy
+    })
+}
+
+function save(toy) {
+    const savedToy = (toy._id) ? storageService.put(KEY, toy) : storageService.post(KEY, toy)
+    return savedToy
+}
+
+function getEmptyToy() {
+    return {
+        name: '',
+        price: 0,
+        labels: [],
+        createdAt: null,
+        inStock: true,
+    }
+}
 
 
 // Create Test Data:
-
-
-
 function _createToys() {
     var toys = JSON.parse(localStorage.getItem(KEY))
     if (!toys || !toys.length) {
@@ -32,8 +59,6 @@ function _createToys() {
     }
     return toys;
 }
-
-
 function _createToy(name, labels) {
     return {
         _id: utilService.makeId(),
@@ -44,3 +69,22 @@ function _createToy(name, labels) {
         inStock: true,
     }
 } 
+
+function _createreviews() {
+    return [
+        _createreview('Elisha T.'),
+        _createreview('Alexy S.'),
+        _createreview('Meital T.'),
+        _createreview('Dina A.')
+    ]
+}
+
+function _createreview(name) {
+    const reviews = ['Greate toy, my son loved it!', 'It\'s OK, I would choose something else next time', 'Wonderful', 'Love it!', 'Very Recomanded']
+    return {
+        _id: utilService.makeId(),
+        name,
+        rate: utilService.getRandomInt(1, 5),
+        review: reviews[utilService.getRandomInt(0, 4)],
+    }
+}
